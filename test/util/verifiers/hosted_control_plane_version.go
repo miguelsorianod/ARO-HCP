ef0f1b19b5b4a0415c36e782700ec263a51ca311
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2026-03-26 19:05:17 +0100

    e2e: add control plane minor upgrade basic e2e test.
    This add a test case for minor upgrades of the control plane.
    It states minor upgrade from a x.y to x.y+1 version.
    
    The frontend allows already patching the version if the subscription has a specific AFEC flag; this should be the case for our testing subscription and the idea is to encover all the issues and fix them.
    
    Future improvements:
    - Update the test to include creating a node pool and upgrading of the nodepool in the new minor.
    - Test workload is disrupted

--- test/util/verifiers/hosted_control_plane_version.go
+++ test/util/verifiers/hosted_control_plane_version.go
@@ -25,6 +25,9 @@ import (
 	"k8s.io/client-go/rest"
 
 	configv1client "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
+
+	"github.com/Azure/ARO-HCP/internal/api"
+	"github.com/Azure/ARO-HCP/test/util/framework"
 )
 
 type verifyHostedControlPlaneZStreamUpgradeOnly struct {
@@ -51,7 +54,8 @@ func (v verifyHostedControlPlaneZStreamUpgradeOnly) Verify(ctx context.Context, 
 		return fmt.Errorf("failed to get clusterversion %q: %w", "version", err)
 	}
 
-	ginkgo.GinkgoLogr.Info("Retrieved openshift cluster version history", "ocp version history", clusterVersion.Status.History)
+	ginkgo.GinkgoLogr.Info("Retrieved openshift cluster version history",
+		"history", framework.SummarizeClusterVersionHistory(clusterVersion.Status.History))
 
 	var sawUpgrade bool
 	for _, history := range clusterVersion.Status.History {
@@ -82,4 +86,61 @@ func (v verifyHostedControlPlaneZStreamUpgradeOnly) Verify(ctx context.Context, 
 // major.minor as initialVersion.
 func VerifyHostedControlPlaneZStreamUpgradeOnly(initialVersion string) HostedClusterVerifier {
 	return verifyHostedControlPlaneZStreamUpgradeOnly{initialVersion: initialVersion}
+}
+
+type verifyHostedControlPlaneYStreamUpgrade struct {
+	targetMinor   string
+	previousMinor string
+}
+
+func (v verifyHostedControlPlaneYStreamUpgrade) Name() string {
+	return fmt.Sprintf("VerifyHostedControlPlaneYStreamUpgrade(previousMinor=%s, targetMinor=%s)", v.previousMinor, v.targetMinor)
+}
+
+func (v verifyHostedControlPlaneYStreamUpgrade) Verify(ctx context.Context, adminRESTConfig *rest.Config) error {
+	configClient, err := configv1client.NewForConfig(adminRESTConfig)
+	if err != nil {
+		return fmt.Errorf("failed to create config client: %w", err)
+	}
+
+	clusterVersion, err := configClient.ClusterVersions().Get(ctx, "version", metav1.GetOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to get clusterversion %q: %w", "version", err)
+	}
+
+	ginkgo.GinkgoLogr.Info("clusterversion status after y-stream upgrade",
+		"history", framework.SummarizeClusterVersionHistory(clusterVersion.Status.History))
+
+	parsedPreviousMinor := api.Must(semver.ParseTolerant(v.previousMinor))
+	parsedTargetMinor := api.Must(semver.ParseTolerant(v.targetMinor))
+
+	var previousMinorFound, targetMinorFound bool
+	for _, historyEntry := range clusterVersion.Status.History {
+		if len(historyEntry.Version) == 0 {
+			continue
+		}
+		version, err := semver.ParseTolerant(historyEntry.Version)
+		if err != nil {
+			return fmt.Errorf("parse clusterversion history version %q: %w", historyEntry.Version, err)
+		}
+		if version.Major == parsedPreviousMinor.Major && version.Minor == parsedPreviousMinor.Minor {
+			previousMinorFound = true
+		}
+		if version.Major == parsedTargetMinor.Major && version.Minor == parsedTargetMinor.Minor {
+			targetMinorFound = true
+		}
+	}
+	if !previousMinorFound {
+		return fmt.Errorf("clusterversion status.history has no version in previous minor %q", v.previousMinor)
+	}
+	if !targetMinorFound {
+		return fmt.Errorf("clusterversion status.history has no version in target minor %q", v.targetMinor)
+	}
+	return nil
+}
+
+// VerifyHostedControlPlaneYStreamUpgrade returns a verifier that clusterversion status.history
+// contains at least one parseable version in previousMinor and at least one in targetMinor.
+func VerifyHostedControlPlaneYStreamUpgrade(previousMinor, targetMinor string) HostedClusterVerifier {
+	return verifyHostedControlPlaneYStreamUpgrade{previousMinor: previousMinor, targetMinor: targetMinor}
 }
